b0dc2da88d11e8d66428477f78b132e0d3c1d50c
atiasultani <160133182+atiasultani@users.noreply.github.com>
2025-05-21 02:44:03 +0500

    done

--- 12-static-method.py
+++ 12-static-method.py
@@ -2,3 +2,13 @@
 # Assignment:
 # Create a class TemperatureConverter with a 
 # static method celsius_to_fahrenheit(c) that returns the Fahrenheit value.
+
+class TemperatureConverter:
+  
+    @staticmethod
+    def celsius_to_fahrenheit(c) :
+            return(c*9/5)+32
+    
+celsius=25
+f=TemperatureConverter.celsius_to_fahrenheit(celsius)
+print(f"celsius is {celsius} and the fahrenheit is {f}")
---
+++ 13-composition.py
@@ -0,0 +1,6 @@
+# 13. Composition
+# Assignment:
+# Create a class Engine and a class Car. 
+# Use composition by passing an Engine object to the Car class during initialization.
+# Access a method of the Engine class via the Car class.
+
